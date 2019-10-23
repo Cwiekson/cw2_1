e22a62a09a0a23046a6e14c2cfb7f3ac8e906c1b
Bartek <Bartek@DESKTOP-NC7V43R>
2019-10-24 00:15:18 +0200

    dyrektor

---
+++ cw2_1/Osoba.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cw2_1
+{
+    class Osoba
+    {
+        public string imie;
+        public string nazwisko;
+        public int rokUrodzenia;
+        public int waga;
+        public int wzrost;
+        public bool okulary;
+        internal plec value;
+         public enum plec {K,M };
+        public int obliczWiek()
+        {
+            return DateTime.Now.Year - rokUrodzenia;
+        }
+        public void Przedrostek()
+        {
+            if (value == plec.M) Console.WriteLine("Dyrektorem jest Pan " + this.imie + " " + this.nazwisko);
+            else Console.WriteLine("Dyrektorem jest Pani " + this.imie + " " + this.nazwisko);
+        }
+
+
+    }
+}
--- cw2_1/Program.cs
+++ cw2_1/Program.cs
@@ -6,7 +6,45 @@ namespace cw2_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //    KlasaMetodyPrzeciazone obiekt = new KlasaMetodyPrzeciazone();
+            //    string zwroconaWartosc = null;
+
+            //    zwroconaWartosc = obiekt.jakiTyp();
+            //    Console.WriteLine(zwroconaWartosc);
+
+            //    zwroconaWartosc = obiekt.jakiTyp(123);
+            //    Console.WriteLine(zwroconaWartosc);
+            //    Console.ReadKey();
+            Osoba dyrektor = new Osoba();
+            dyrektor.imie = "Jan";
+            dyrektor.nazwisko = "Kowalski";
+            dyrektor.rokUrodzenia = 1967;
+            dyrektor.waga = 86;
+            dyrektor.wzrost = 180;
+            dyrektor.okulary = true;
+            dyrektor.value = Osoba.plec.M;
+            // Console.WriteLine("{0} {1} {2} {3} {4} {5} {6}", dyrektor.imie, dyrektor.nazwisko, dyrektor.rokUrodzenia,dyrektor.waga,dyrektor.wzrost,dyrektor.okulary,dyrektor.value);
+            Console.WriteLine("{0} {1} {2}",dyrektor.imie,dyrektor.nazwisko, dyrektor.obliczWiek());
+            dyrektor.Przedrostek();
+            Console.ReadKey();
         }
     }
-}
+    //class KlasaMetodyPrzeciazone
+    //{
+
+    //    public string jakiTyp()
+    //    {
+    //        return "Nie podano argumentów metody";
+    //    }
+
+
+    //    public string jakiTyp(int zmienna)
+    //    {
+    //        return "Liczba całkowita";
+    //    }
+
+    //    public string jakiTyp(string zmienna)
+    //    {
+    //        return "Łańcuch znaków";
+    //    }
+    }
